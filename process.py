eb938d4e814683c0af57bc3a916a0db04a99ef16
yuedayu <yuedayu@sensetime.com>
2019-08-21 22:40:09 +0800

    enhance night light

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ sys.path.append("/opt/ros/kinetic/lib/python2.7/dist-packages")
 sys.path.append("/home/sensetime/code/repo_pro/senseauto/build/devel/lib/python2.7/dist-packages")
 
 import cv2
+import numpy as np
 
 input_image = cv2.imread(sys.argv[1])
 input_image[:200, :470, :] = 0
@@ -11,6 +12,25 @@ input_image[:200, :470, :] = 0
 height, width, channel = input_image.shape
 
 input_image[(height - 200):, (width - 200):, :] = 0
+
+yellow_lower = np.array([26, 60, 45], np.uint8)
+yellow_upper = np.array([34, 255, 255], np.uint8)
+hsv = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)
+
+yellow = cv2.inRange(hsv, yellow_lower, yellow_upper)
+
+night_light = np.zeros((height, width, 3), dtype=np.uint8)
+night_light[yellow == 255] = np.array([0, 170, 240], dtype=np.uint8)
+night_light = cv2.blur(night_light, (5, 5))
+
+weight = night_light[:, :, 2] / 255. / 1.5
+night_light[:, :, 0] = night_light[:, :, 0] * weight
+night_light[:, :, 1] = night_light[:, :, 1] * weight
+night_light[:, :, 2] = night_light[:, :, 2] * weight
+input_image[:, :, 0] = input_image[:, :, 0] * (1 - weight)
+input_image[:, :, 1] = input_image[:, :, 1] * (1 - weight)
+input_image[:, :, 2] = input_image[:, :, 2] * (1 - weight)
+input_image = input_image + night_light
 
 input_image = cv2.copyMakeBorder(input_image, 120, 50, 0, 0, cv2.BORDER_CONSTANT)
 
--- run.sh
+++ run.sh
@@ -4,5 +4,8 @@ wget "http://img.nsmc.org.cn/CLOUDIMAGE/FY4A/MTCC/FY4A_DISK.JPG" -O /home/senset
 
 /usr/bin/python /home/sensetime/code/wallpaper/process.py /home/sensetime/code/wallpaper/data/origin.jpg /home/sensetime/code/wallpaper/data/process.jpg
 
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
 /usr/bin/gsettings set org.gnome.desktop.background picture-uri "file:///home/sensetime/code/wallpaper/data/process.jpg"
 /usr/bin/gsettings set org.gnome.desktop.background picture-options spanned
